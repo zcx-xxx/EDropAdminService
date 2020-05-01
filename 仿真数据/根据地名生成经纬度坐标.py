f0295fadf7aabd1bb3ee517b2b8ae3a8baf4e698
zcxhaha <1307125643@qq.com>
2020-05-02 02:12:43 +0800

    仿真数据

---
+++ 仿真数据/根据地名生成经纬度坐标.py
@@ -0,0 +1,44 @@
+import xlrd
+import xlwt
+import requests
+import json
+import time
+
+def geocodeB(address):
+    """
+    @ address: 名称字符串
+    @ 返回值：经度，纬度
+    """
+    base_url = "http://api.map.baidu.com/geocoder?address={address}&output=json&key=8BB7F0E5C9C77BD6B9B655DB928B74B6E2D838FD".format(address=address)
+    # print(address)
+    response = requests.get(base_url)
+    # print(response.status_code)
+    print(len(response.content))
+    if (response.status_code == 200) and (len(response.content) < 300):
+        answer = response.json()
+        latitude = answer['result']['location']['lng']
+        longitude = answer['result']['location']['lat']
+        return latitude, longitude
+    else:
+        return geocodeB(address)
+
+if __name__ == "__main__":
+    # 打开文件
+    excel_file = xlrd.open_workbook('C:/Users/13071/Desktop/小区坐标.xlsx')
+    # 根据索引打开指定的工作表
+    read_sheet = excel_file.sheet_by_index(0)
+    # 获取行数
+    rows = read_sheet.nrows
+
+    # 创建一个新的工作表
+    write_book = xlwt.Workbook(encoding='utf-8')
+    write_sheet = write_book.add_sheet("sheet1")
+
+    for i in range(0, rows):
+        address = read_sheet.cell(i, 2).value + " " + read_sheet.cell(i, 3).value + " " + read_sheet.cell(i, 4).value
+        latitude_, longitude_ = geocodeB(address)
+        data = (latitude_, longitude_, read_sheet.cell(i, 2).value, read_sheet.cell(i, 3).value, read_sheet.cell(i, 4).value)
+        for j in range(len(data)):
+            write_sheet.write(i, j, data[j])
+    
+    write_book.save('C:/Users/13071/Desktop/小区坐标.xls')
---
+++ 仿真数据/通过基础坐标随机生成附近地址坐标.py
@@ -0,0 +1,58 @@
+import random
+import math
+import xlrd
+import xlwt
+import json
+
+#  参数含义 
+# base_log：经度基准点， 
+# base_lat：维度基准点， 
+# radius：距离基准点的半径
+def generate_random_gps(base_log=None, base_lat=None, radius=None):
+    radius_in_degrees = radius / 111300
+    u = float(random.uniform(0.0, 1.0))
+    v = float(random.uniform(0.0, 1.0))
+    w = radius_in_degrees * math.sqrt(u)
+    t = 2 * math.pi * v
+    x = w * math.cos(t)
+    y = w * math.sin(t)
+    longitude = y + base_log
+    latitude = x + base_lat
+    # 这里是想保留14位小数
+    loga = '%.14f' % longitude
+    lata = '%.14f' % latitude
+    return loga, lata
+
+# 114.529344,38.003631 软件学院坐标
+if __name__ == "__main__":
+    # 打开文件
+    excel_file = xlrd.open_workbook('C:/Users/13071/Desktop/小区坐标.xls')
+    # 根据索引打开指定的工作表
+    read_sheet = excel_file.sheet_by_index(0)
+    # 获取行数
+    rows = read_sheet.nrows
+    # 创建一个新的工作表
+    # write_book = xlwt.Workbook(encoding='utf-8')
+    # write_sheet = write_book.add_sheet("sheet2")
+
+    tot = 0
+    list = []
+    for i in range(rows):
+        address = {}
+        for k in range(0, 5):
+            address[k] = read_sheet.cell(i, k).value
+        longitude = address[0]
+        latitude = address[1]
+
+        for j in range(100):
+            address[0], address[1] = generate_random_gps(base_log=longitude, base_lat=latitude, radius=150)
+            address[5] = random.randint(20, 300)
+            # for p in range(len(address)):
+            #     write_sheet.write(tot, p, address[p])
+            # write_sheet.write(tot, 5, random.randint)
+            list.append({'coord':[address[0], address[1]], 'elevation':address[5]})
+            tot += 1
+
+    with open('data.json', 'w') as f:
+        json.dump(list, f)
+    # write_book.save('C:/Users/13071/Desktop/random_address.xls')
